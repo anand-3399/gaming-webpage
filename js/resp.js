d6602db3b89aacb7130d83bb1b67735543a0cdf5
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-02-09 01:57:01 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+mid = document.querySelector('.mid')
+navbar = document.querySelector('.navbar')
+// rightNav = document.querySelector('.rightNav')
+
+
+burger.addEventListener('click',()=>{
+    // rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('v-class-resp');
+    mid.classList.toggle('h-nav-resp');
+})
