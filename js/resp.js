684b050cef829a0f90ac7d1db0e902c3aec18785
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-04-06 13:10:20 +0530

    Added Button.

--- js/resp.js
+++ js/resp.js
@@ -1,11 +1,11 @@
 burger = document.querySelector('.burger')
 mid = document.querySelector('.mid')
 navbar = document.querySelector('.navbar')
-// rightNav = document.querySelector('.rightNav')
+rightNav = document.querySelector('.rightNav')
 
 
 burger.addEventListener('click', () => {
-    // rightNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('v-class-resp');
     mid.classList.toggle('h-nav-resp');
 })
